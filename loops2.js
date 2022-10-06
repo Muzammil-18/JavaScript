417cfd8cf1d6994d97e2451b403ca1e5e5f675bb
Muzammil-18 <muzammilmoosani04@gmail.com>
2022-10-06 23:19:22 +0500

    files added

--- loops2.js
+++ loops2.js
@@ -1,13 +1,19 @@
 let i = 1
 
 
-while (i <= 5)
-{
-    let j = 1
-    while (j<=10)
-    {
-        console.log(i,"X",j,"=",i*j)
-        j++
-    }
-    i++
-}
+// while (i <= 5)
+// {
+//     let j = 1
+//     while (j<=10)
+//     {
+//         console.log(i,"X",j,"=",i*j)
+//         j++
+//     }
+//     i++
+// }
+
+
+for(let h=1;h<=10;h++)
+for(let z=1;z<=5;z++)
+console.log(z,"X",h,"=",z*h)
+
